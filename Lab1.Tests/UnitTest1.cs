99b66524efe13d96501f5545ad9441cc150e83b7
Карвацький Володимир <54777009+karvatskyvova@users.noreply.github.com>
2024-10-10 13:35:48 +0300

    Lab1 v1.1 more tests

--- Lab1.Tests/UnitTest1.cs
+++ Lab1.Tests/UnitTest1.cs
@@ -21,6 +21,36 @@ public class UnitTest
         // Assert
         Assert.Equal(5, maxStrings); // Очікуваний результат
     }
+    [Fact]
+    public void Test3()
+    {
+        // Arrange
+        string[] input = { "3", "4" }; // Фіктивні дані
+        // Act
+        long maxStrings = CalculateMaxStrings(input);
+        // Assert
+        Assert.Equal(64, maxStrings); // Очікуваний результат
+    }
+    [Fact]
+    public void Test4()
+    {
+        // Arrange
+        string[] input = { "1", "10" }; // Фіктивні дані
+        // Act
+        long maxStrings = CalculateMaxStrings(input);
+        // Assert
+        Assert.Equal(10, maxStrings); // Очікуваний результат
+    }
+    [Fact]
+    public void Test5()
+    {
+        // Arrange
+        string[] input = { "2", "8" }; // Фіктивні дані
+        // Act
+        long maxStrings = CalculateMaxStrings(input);
+        // Assert
+        Assert.Equal(64, maxStrings); // Очікуваний результат
+    }
 
     private long CalculateMaxStrings(string[] input)
     {
