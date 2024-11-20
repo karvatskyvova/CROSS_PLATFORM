6012c5066a5784c4e0a7dc35d504c27690ecd723
Карвацький Володимир <54777009+karvatskyvova@users.noreply.github.com>
2024-11-20 20:00:53 +0200

    v2.0

---
+++ Lab3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    public const int INF = 30000; // Значення для недосяжних вершин
+
+    public static int[] BellmanFord(int n, int[,] edges)
+    {
+        int[] dist = new int[n];
+        Array.Fill(dist, INF); // Заповнюємо всі відстані INF
+        dist[0] = 0; // Відстань від вершини 1 до себе - 0
+
+        // N-1 разів релаксуємо всі ребра
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < edges.GetLength(0); j++)
+            {
+                int u = edges[j, 0] - 1; // Початок ребра
+                int v = edges[j, 1] - 1; // Кінець ребра
+                int weight = edges[j, 2]; // Вага ребра
+
+                if (dist[u] != INF && dist[u] + weight < dist[v])
+                {
+                    dist[v] = dist[u] + weight;
+                }
+            }
+        }
+
+        return dist;
+    }
+
+    public static void Main(string[] args)
+    {
+        var input = File.ReadAllLines("Lab3//INPUT.TXT");
+        var firstLine = input[0].Split();
+        int n = int.Parse(firstLine[0]);
+        int m = int.Parse(firstLine[1]);
+
+        int[,] edges = new int[m, 3];
+
+        for (int i = 0; i < m; i++)
+        {
+            var edge = input[i + 1].Split();
+            edges[i, 0] = int.Parse(edge[0]);
+            edges[i, 1] = int.Parse(edge[1]);
+            edges[i, 2] = int.Parse(edge[2]);
+        }
+
+        var result = BellmanFord(n, edges);
+        File.WriteAllText("OUTPUT.TXT", string.Join(" ", result));
+    }
+}
